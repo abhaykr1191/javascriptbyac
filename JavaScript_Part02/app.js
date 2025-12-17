ad0902a5c7b023c1a78bc55cf15ad9002843efd8
Abhay <abhaykr1190@gmail.com>
2025-12-17 10:10:40 +0000

    Javascript 2

---
+++ JavaScript_Part02/app.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+// console.log("The total price is :", pencilPrice + erasorPrice," Rupees. ");
+
+let output = "The total price is : " + (pencilPrice + erasorPrice) + " Rupees.";
+console.log(output);
+
+
+//  Template Literals -> Thet are used to add embedded expression in a string.
+
+let a = 10;
+let b = 10;
+console.log(`You pay ${a+b} rupees`);
+console.log("Price is", a+b, "rupees");
+
