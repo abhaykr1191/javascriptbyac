502cd53ffe4bd92eecd4381d5c12ec0746aa1328
Abhay <abhaykr1190@gmail.com>
2025-12-19 12:07:59 +0000

    191225

--- JavaScript_Part03/app.js
+++ JavaScript_Part03/app.js
@@ -2,7 +2,36 @@
 // Methods - actions that can be performed on objects
 
 //Format
-let stringName = ""
 // stringName.method()
+
+// let password = prompt("Enter Password")
+// let npass = password.trim();
+// console.log(password);
+
+// Strings are Immutable in JS
+// No changes can be made to strings.
+// Whenever we try to make a change, a new string is created and old one remains same.
+
+// password.toUpperCase()
+// password.toLowerCase()
+
+// String method with argument -> Argument is some value that we pass to the metod.
+// stringName.method(arg)
+let str = "hcfgvjlgcgh.ds  "
+str.indexOf("s") // first index of the arg
+
+// method chaining
+let msg = "   hello "
+
+// let newMsg = msg.trim();
+// newMsg = newMsg.toUpperCase();
+// console.log(newMsg);
+
+msg = msg.trim().toUpperCase()
+console.log(msg);
+
+// slice ->  Returns a part of the original string as a new string
+
+
 
 
